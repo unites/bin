4e6f9810a655591d2d1f51d34a25ee5458f1bcc1
unites <admin@sim.pub>
2022-05-26 13:35:41 -0500

    init commit

---
+++ lock.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+i3lock -c 330000
+#exec xautolock -detectsleep -time 1 -locker "i3lock -c 330000" -notify 30 -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
